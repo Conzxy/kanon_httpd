f3b7bf9ad91bd6e420559338529d5953fe4c271c
Conzxy <Conzxy@1967933844@qq.com>
2022-03-28 00:02:08 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+mkdir build && cd build && \
+# Options:
+# -DBUILD_ALL_TESTS=ON(default: OFF)
+# -DBUILD_STATIC_LIBS=ON(default: OFF)
+# -G Ninja(default: Unix Makefiles), ninja need install
+# -DCMAKE_BUILD_TYPE=Debug/...(default: Release)
+cmake .. && \
+cmake --build . -j 2 && \
+cmake -DGEN_PLUGIN=ON ..
+# Then, you can check ./lib/ if has three *.so files
+# , ../resources/contents/adder and ../bin/httpd_kanon
+# if does exists, then run the ../bin/httpd_kanon
+# (You should can the working directory to ../bin and 
+#  modify the ../bin/kanon_httpd.conf)
