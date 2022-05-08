d3177dc1afc33e3aacb8168478971f566610bbfe
Conzxy <Conzxy@1967933844@qq.com>
2022-05-08 22:02:12 +0800

    add: support mapped address shared among threads

--- src/http2/http_server2.cc
+++ src/http2/http_server2.cc
@@ -7,9 +7,12 @@
 #include <kanon/util/optional.h>
 
 #include "unix/fd_wrapper.h"
-#include "http_session.h"
+#include "unix/mmap.h"
+
+#include "http2/http_session.h"
 
 using namespace kanon;
+using namespace unix;
 
 namespace http {
 
@@ -23,12 +26,24 @@ HttpServer::HttpServer(EventLoop* loop, InetAddr const& addr)
       session->Setup();
       LOG_DEBUG << "[Session #" << session->GetId() << "] constructed";
       conn->SetContext(std::move(session));
-      LOG_DEBUG << "Connection: " << conn.get() << "; Session: " << session.get();
     }
     else {
-      auto session = *kanon::AnyCast<std::shared_ptr<HttpSession>>(conn->GetContext());
+      // Here, Don't use:
+      //   auto session = *kanon::AnyCast<std::shared_ptr<HttpSession>>(conn->GetContext());
+      //   session.reset();
+      // This do nothing, the ref-count no change.
+      //   session --> connection
+      //   connection --> session (dead lock)
+      // Solution:
+      // 1. Use auto&
+      // 2. Set empty context to desctory shared_ptr
+      //    conn->SetContext();
+      auto& session = *kanon::AnyCast<std::shared_ptr<HttpSession>>(conn->GetContext());
       LOG_DEBUG << "[Session #" << session->GetId() << "] destroyed";
-      LOG_DEBUG << "Connection: " << conn.get() << "; Session: " << session.get();
+      conn->SetWriteCompleteCallback(WriteCompleteCallback());
+      session->Teardown();
+      LOG_DEBUG << "ref-count = " << session.use_count();
+
       session.reset();
     }
 
@@ -70,6 +85,33 @@ kanon::optional<off_t> HttpServer::SearchOffset(uint64_t session)
 
 std::shared_ptr<int> HttpServer::GetFd(std::string const& path)
 {
+  /* 
+   * The race condition between GetFd() and deleter:
+   * Call the deleter but lock is preempted by other thread calling GetFd()
+   * 1. Deleter must ensure remove entry when the ref-count == 0
+   *    Otherwise, deleter may remove a new entry with same key which created in 
+   *    other thread, then Call the GetFd() will get different entry and all state is missing.
+   *    +---------------------+---------------------+
+   *    | Thread 1            | Thread2             |
+   *    | deleter C           |                     |
+   *    |                     | GetFd() A           |
+   *    | find success        |                     |
+   *    | remove A            |                     | <--- Must check ref-count!
+   *    |                     | GetFd() B(new entry)|
+   *    +---------------------+---------------------+
+   *    Solution:
+   *    Use std::weak_ptr::expired()
+   *    Don't use std::weak_ptr::lock()
+   * 2. Don't use assert(find != end) in the deleter
+   *    +---------------------+---------------------+
+   *    | Thread 1            | Thread2             |
+   *    | deleter A           |                     |
+   *    |                     | GetFd() B           |
+   *    |                     | deleter B           |
+   *    | assert failed       |                     |
+   *    +---------------------+---------------------+
+   *    The scenario maybe occurred when GetFd() don't switch to other thread
+   */
   MutexGuard guard(mutex_);
   
   auto& wptr = fd_map_[path];
@@ -81,7 +123,7 @@ std::shared_ptr<int> HttpServer::GetFd(std::string const& path)
       LOG_SYSERROR << "Failed to open " << path;
       return nullptr;
     }
-
+    
     sp.reset(new int(fd), [path, this](int* p) {
       // p maybe nullptr
       if (p) {
@@ -94,9 +136,9 @@ std::shared_ptr<int> HttpServer::GetFd(std::string const& path)
         }
         unix::FDWrapper wrapper(*p);
         }
-      }
 
-      delete p;
+        delete p;
+      }
     });
 
     wptr = sp;
@@ -104,5 +146,51 @@ std::shared_ptr<int> HttpServer::GetFd(std::string const& path)
   
   return sp;
 }
+
+std::shared_ptr<char*> HttpServer::GetAddr(std::string const& pathname, size_t len) {
+  MutexGuard guard(mutex_addr_);
+
+  auto& wp = addr_map_[pathname];
+  auto sp = wp.lock();
+
+  if (!sp) {
+    int fd = ::open(pathname.c_str(), O_RDONLY);
+    if (fd < 0) {
+      LOG_SYSERROR << "Failed to open " << pathname;
+      return nullptr;
+    }
+
+    auto addr = Mmap(fd, len, PROT_READ, MAP_PRIVATE);
+
+    if (!addr) {
+      LOG_SYSERROR << "Failed to call mmap() with " << pathname;
+      ::close(fd);
+      return nullptr;
+    }
+
+    sp.reset(new char*(addr), [this, pathname, len](char** p) {
+      if (p) {
+        {
+          MutexGuard guard(mutex_addr_);
+
+          auto iter = addr_map_.find(pathname);
+
+          if (iter != addr_map_.end() && iter->second.expired()) {
+            addr_map_.erase(iter);
+          }
+
+          Munmap(*p, len);
+        }
+
+        delete p;      
+      }
+    });
+
+    wp = sp;
+  }
+
+  return sp;
+}
+
 
 } // namespace http
--- src/http2/http_server2.h
+++ src/http2/http_server2.h
@@ -26,9 +26,13 @@ private:
   // Cache factory method
   // @see Modern Effective C++ Item 21
   std::shared_ptr<int> GetFd(std::string const& path);
+  std::shared_ptr<char*> GetAddr(std::string const& path, size_t len);
 
   kanon::MutexLock mutex_;
   std::unordered_map<std::string, std::weak_ptr<int>> fd_map_;
+
+  kanon::MutexLock mutex_addr_;
+  std::unordered_map<std::string, std::weak_ptr<char*>> addr_map_;
 
   kanon::RWLock lock_offset_;
   std::unordered_map<uint64_t, off_t> offset_map_;
