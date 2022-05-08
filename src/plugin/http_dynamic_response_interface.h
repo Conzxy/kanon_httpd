d6401d52f97cb4f91a3c11b7ea15a4de9a4d31c9
Conzxy <Conzxy@1967933844@qq.com>
2022-05-08 22:01:08 +0800

    modify: add version_ and conn_

--- src/plugin/http_dynamic_response_interface.h
+++ src/plugin/http_dynamic_response_interface.h
@@ -1,7 +1,8 @@
 #ifndef HTTP_DYNAMIC_REPONSE_INTERFACE_H
 #define HTTP_DYNAMIC_REPONSE_INTERFACE_H
 
-#include "kanon/util/noncopyable.h"
+#include <kanon/util/noncopyable.h>
+#include <kanon/net/tcp_connection.h>
 
 #include "common/types.h"
 #include "common/http_response.h"
@@ -12,10 +13,15 @@ class HttpDynamicResponseInterface {
 public:
   HttpDynamicResponseInterface() = default;  
   virtual ~HttpDynamicResponseInterface() = default;
-  virtual HttpResponse GenResponseForGet(ArgsMap const& args) = 0;
-  virtual HttpResponse GenResponseForPost(std::string const& body) = 0;
+  virtual void GenResponseForGet(ArgsMap const& args, HttpResponse& response) = 0;
+  virtual void GenResponseForPost(std::string const& body, HttpResponse& response) = 0;
 
-private:
+  void SetVersion(HttpVersion ver) noexcept { version_ = ver; }
+  void SetConnection(kanon::TcpConnectionPtr const& conn) { conn_ = conn; }
+
+protected:
+  kanon::TcpConnectionPtr conn_;
+  HttpVersion version_; 
   DISABLE_EVIL_COPYABLE(HttpDynamicResponseInterface)
 };
 
